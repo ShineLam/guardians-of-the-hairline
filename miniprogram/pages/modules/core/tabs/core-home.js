335d115924a20aa93d4317874e01eb876c204422
linyu <ethan_lamv@163.com>
2022-06-30 10:18:57 +0800

    upd

--- miniprogram/pages/modules/core/tabs/core-home.js
+++ miniprogram/pages/modules/core/tabs/core-home.js
@@ -1,5 +1,16 @@
 const { mixin, api, util, conf, isEqualList } = require('../../../../utils/mixin.js')
-const nav = ['popup', 'dialog', 'preview', 'pick-time', 'plate-keybod', 'num-keybod', 'calc-keybod', 'mbl-valid', 'user-auth', 'cpns', 'upload-img']
+const nav = [
+  { title: 'popup', ico: 'ico-popup', methods: 'open', params: {} },
+  { title: 'dialog', ico: 'ico-dialog', methods: 'open', params: {} },
+  { title: 'preview', ico: 'ico-preview', methods: 'open', params: {} },
+  { title: 'pick-time', ico: 'ico-time', methods: 'open', params: {} },
+  { title: 'plate-keybod', ico: 'ico-k-plate', methods: 'open', params: {} },
+  { title: 'num-keybod', ico: 'ico-k-num', methods: 'open', params: {} },
+  { title: 'calc-keybod', ico: 'ico-k-calc', methods: 'open', params: {} },
+  { title: 'mbl-valid', ico: 'ico-phone', methods: 'open', params: {} },
+  { title: 'user-auth', ico: 'ico-auth', methods: 'open', params: {} },
+  { title: 'cpns', ico: 'ico-cpns', methods: 'open', params: {} },
+]
 
 Component(mixin.component({
   options: {
@@ -23,8 +34,8 @@ Component(mixin.component({
     onNav(e) {
       let i = e.currentTarget.dataset.i
       let item = nav[i]
-      let com = this.selectComponent('#' + item)
-      com.open()
+      let com = this.selectComponent('#' + item.title)
+      com[item.methods](item.params)
     },
     onShow() {
       let time = Date.now()
